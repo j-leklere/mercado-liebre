9f2345a7e09877479cd68bdad5d45a1f61b72f94
j-leklere <jleklere@yahoo.com.ar>
2022-08-10 19:57:55 -0300

    actualizando archivos

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const { get } = require('http');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath))
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
 app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+  console.log("Servidor corriendo en el puerto 3000");
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
