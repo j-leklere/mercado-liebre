8676422cbaf8a799e591669c54646d9414d060e8
j-leklere <jleklere@yahoo.com.ar>
2022-08-22 17:50:29 -0300

    agregando estilos

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(3030, () => {
-  console.log("Servidor corriendo en el puerto 3030");
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
 });
 
 app.get("/", (req, res) => {
