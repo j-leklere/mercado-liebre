05804f88b8d91d1fda9747e1f62f13e61185f753
j-leklere <jleklere@yahoo.com.ar>
2022-08-24 17:32:16 -0300

    creando rutas y agregando register y login

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+  console.log("Te registraste correctamente");
+  res.redirect("/");
+});
+
+app.get("/login", (req, res) => {
+  console.log("Te logeaste correctamente");
+  res.redirect("/");
+});
